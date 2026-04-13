3e1562a26782a4dc940c4421d9192d95ea498f4f
Pablo Di Taranto <103513283+PDiTaranto@users.noreply.github.com>
2026-04-13 11:45:40 -0300

    Create custom ability system component

---
+++ Source/ARPG/Public/AbilitySystem/Components/ARPGPbilitySystemComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "ARPGPbilitySystemComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARPG_API UARPGPbilitySystemComponent : public UAbilitySystemComponent
+{
+	GENERATED_BODY()
+	
+};
