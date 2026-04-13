92b30f04788a8a9002952bcc85b60b8ea6060eed
Pablo Di Taranto <103513283+PDiTaranto@users.noreply.github.com>
2026-04-13 13:36:14 -0300

    Create jump gameplay ability

---
+++ Source/ARPG/Private/AbilitySystem/Abilities/ARPGJumpAbility.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "AbilitySystem/Abilities/ARPGJumpAbility.h"
+#include "Player/Characters/ARPGPlayerCharacter.h"
+
+UARPGJumpAbility::UARPGJumpAbility()
+{
+	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+}
+
+void UARPGJumpAbility::ActivateAbility(
+	const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo,
+	const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	AARPGPlayerCharacter* PlayerCharacter = GetARPGPlayerCharacterFromActorInfo();
+	if (!PlayerCharacter)
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+		return;
+	}
+
+	PlayerCharacter->Jump();
+
+	EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
+}
+
+void UARPGJumpAbility::EndAbility(
+	const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo,
+	bool bReplicateEndAbility,
+	bool bWasCancelled)
+{
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
---
+++ Source/ARPG/Public/AbilitySystem/Abilities/ARPGJumpAbility.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/ARPGGameplayAbility.h"
+#include "ARPGJumpAbility.generated.h"
+
+UCLASS()
+class ARPG_API UARPGJumpAbility : public UARPGGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	UARPGJumpAbility();
+
+	virtual void ActivateAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		const FGameplayEventData* TriggerEventData) override;
+
+	virtual void EndAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		bool bReplicateEndAbility,
+		bool bWasCancelled) override;
+};
