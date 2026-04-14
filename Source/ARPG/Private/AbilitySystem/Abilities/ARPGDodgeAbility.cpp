2bd0dd403ba45edf1e8f72165b35d2afa6e20c76
Pablo Di Taranto <103513283+PDiTaranto@users.noreply.github.com>
2026-04-14 11:35:11 -0300

    Create dodge gameplay ability

---
+++ Source/ARPG/Private/AbilitySystem/Abilities/ARPGDodgeAbility.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "AbilitySystem/Abilities/ARPGDodgeAbility.h"
+
+UARPGDodgeAbility::UARPGDodgeAbility()
+{
+	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+}
+
---
+++ Source/ARPG/Public/AbilitySystem/Abilities/ARPGDodgeAbility.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/ARPGGameplayAbility.h"
+#include "ARPGDodgeAbility.generated.h"
+
+UCLASS()
+class ARPG_API UARPGDodgeAbility : public UARPGGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	UARPGDodgeAbility();
+};
